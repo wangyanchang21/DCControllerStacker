cf53112199becfac911da98d8d6ffef3e95f011e
DC_Snail <wangyanchang21@163.com>
2017-01-18 18:05:17 +0800

    Add DebugView Category

---
+++ UIView+Debug.h
@@ -0,0 +1,15 @@
+//
+//  UIView+Debug.h
+//  Pray
+//
+//  Created by Tapan Thaker on 29/08/14.
+//  Copyright (c) 2014 Tapan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DebugFrameView.h"
+
+@interface UIView (Debug)
+@property (nonatomic, strong) DebugFrameView *debugFrameView;
+
+@end
